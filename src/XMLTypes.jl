fff81c33645dafb7c79aab0fc5eb03bf1769cbf9
Adam Beckmeyer <adam_gpg@thebeckmeyers.xyz>
2018-08-03 21:45:13 -0400

    Begin writing basics of XML stuff

--- src/XMLTypes.jl
+++ src/XMLTypes.jl
@@ -16,4 +16,48 @@
 
 module XMLTypes
 
+export XMLElement
+
+"Represents an XML element (e.g. `<xml attr=\"string\">content</xml>`)."
+abstract type XMLElement end
+
+export attributes, content
+
+"Returns iterator of `Pair{String,String}` attributes for element."
+attributes(element::XMLElement) = element.attributes
+
+"Returns iterator of `Union{String,XMLElement}` for element contents."
+content(element::XMLElement) = element.content
+
+export dump_attributes, dump
+
+"Turns an iterator of `Pair{String,String}` into standard XML tag attributes."
+function dump_attributes(attributes)::String
+    join(map(p -> "$(p.first)=\"$(p.second)\"", attributes), " ")
+end
+
+"Turns an `XMLElement` into its canonical tag-form representation."
+function dump(element::XMLElement)::String
+    element_name = name(element)
+    attr_str = dump_attributes(attributes(element))
+    # XML tags can be jammed together, and strings require appropriate spaces
+    # TODO: what is max allowed depth of recursion? Problematic for normal uses?
+    content_str = join(dump.(content(element)), "")
+    # If no content, we can use a self-closing tag
+    if length(content(element)) == 0
+        """
+        <$element_name $attr_str />
+        """
+    else
+        """
+        <$element_name $attr_str>
+            $content_str
+        </$element_name>
+        """
+    end#if
+end#function
+
+# `content(element)` is mixed vector of strings and elements to fuse `dump` on
+dump(s::AbstractString) = s
+
 end#module
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,3 +4,17 @@ using XMLTypes
 else
     using Test
 end
+
+struct TestElement <: XMLElement
+    attributes::Vector{Pair{String,String}}
+    content::Vector{Union{String,XMLElement}}
+end#struct
+
+name(::TestElement) = "test"
+
+attr = ["class" => "good", "id" => "4"]
+cont = ["hello world", TestElement([], [])]
+t = TestElement(attr, cont)
+
+@test attributes(t) == attr
+@test content(t) == cont
